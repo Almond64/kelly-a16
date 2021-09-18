dfa65193e2d0467f3b9ae0e4e0cc9a2d8c869637
Almond64 <almond64@knights.ucf.edu>
2021-09-18 17:28:15 -0400

    initial commit

---
+++ src/main/java/Solution16.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your age? ");
+        int age = input.nextInt();
+        //Is user old enough to drive?
+        //(age >= 16) ? System.out.println("You are old enough to legally drive."): System.out.println("You are not old enough to legally drive.");
+        System.out.println((age >= 16) ? "You are old enough to legally drive.": "You are not old enough to legally drive.");
+    }
+}
